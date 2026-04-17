4dc28c9a943edfede04f70cc9e29273132c0f00c
khansameer17275-dev <khansameer17275@gmail.com>
2026-04-17 12:59:59 +0000

    string

---
+++ 01_basics/05_string.js
@@ -0,0 +1,25 @@
+const name = "Sameer"
+const repoCount = 50
+
+// console.log(name + repoCount +" Value");
+
+// console.log('Hello my name is ${name} and my repo count is ${repoCount}');
+
+const gameName = new String('SameerSR')
+
+console.log(gameName[0]);
+console.log(gameName.__proto__);
+
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+console.log(gameName.charAt(2));
+console.log(gameName.indexOf('m'));
+
+const newString = gameName.substring(0, 4)
+console.log(newString);
+
+const anotherString = gameName.slice(-8, 4)
+console.log(anotherString);
+
+
