67d2520e6f5618aef32408b2cae13624602a963c
khansameer17275-dev <khansameer17275@gmail.com>
2026-04-18 08:45:38 +0000

    arrays

--- 02_basics/02_arrays.js
+++ 02_basics/02_arrays.js
@@ -24,3 +24,9 @@ console.log(Array.isArray("Sameer"))
 console.log(Array.from("Sameer"));
 console.log(Array.from({name: "Sameer"})) //interesting
 
+let score1 = 100
+let score2 = 200
+let score3 = 300
+
+console.log(Array.of(score1, score2, score3));
+
