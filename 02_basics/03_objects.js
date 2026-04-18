4acfc79fb56c23b02effd9154babe684e0a0c82c
khansameer17275-dev <khansameer17275@gmail.com>
2026-04-18 09:26:19 +0000

    objects

---
+++ 02_basics/03_objects.js
@@ -0,0 +1,39 @@
+// singleton
+//object.create
+// object literals
+
+
+const mySym = Symbol("Key1")
+
+
+const JsUser = {
+    name: "Sameer",
+    "full name": "Mo Sameer",
+    [mySym]: "mykey1",
+    age: 18,
+    location: "Jaipur",
+    email: "khansameer17275@google.com",
+    isLoggedIn: false,
+    lastLoginDays: ["Monday", "Saturday"]
+}
+
+// console.log(JsUser.email)
+// console.log(JsUser["email"])
+// console.log(JsUser["full name"])
+// console.log(JsUser[mySym])
+
+JsUser.email = "sameer@chatgpt.com"
+//Object.freeze(JsUser)
+JsUser.email = "sameer@microsoft.com"
+console.log(JsUser);
+
+JsUser.greeting = function(){
+    console.log("Hello JS user");
+}
+JsUser.greetingTwo = function(){
+    console.log(`Hello JS user, ${this.name}`);
+}
+
+console.log(JsUser.greeting);
+console.log(JsUser.greetingTwo());
+
