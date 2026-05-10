8243a07ad804c7efa6ae7aa9f811a1b278a659ef
Mo Sameer <khansameer17275@gmail.com>
2026-05-10 19:23:02 +0000

    changes

--- 09_advance_one/promises.js
+++ 09_advance_one/promises.js
@@ -11,3 +11,13 @@ const promiseOne = new Promise(function(resolve, reject){
 promiseOne.then(function(){
     console.log("Promise consumed");
 })
+
+new Promise(function(resolve, reject){
+    setTimeout(function(){
+     console.log("Async task 2");
+     resolve()   
+    }, 1000)
+
+}).then(function(){
+    console.log("Async 2 resolved");
+})
