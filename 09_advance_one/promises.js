75235066162aa89839ffc37fdac27b75f2c04744
Mo Sameer <khansameer17275@gmail.com>
2026-05-11 06:56:07 +0000

    changes

--- 09_advance_one/promises.js
+++ 09_advance_one/promises.js
@@ -51,6 +51,26 @@ const promiseFour = new Promise(function(resolve, reject){
      console.log(username);
 }).catch(function(error){
     console.log(error);
+}).finally(() => console.log("The promise is either resolved or rejected")
+)
+
+const promiseFive = new Promise(function(resolve, reject) {
+    setTimeout(function(){
+        let error = true 
+        if (!error) {
+            resolve({username: "javascript", password: "123"})
+        } else {
+            reject('ERROR: JS went wrong')
+        }
+    }, 1000)
 })
+
+async function consumePromiseFive(){
+   const response = await promiseFive
+   console.log(response);
+   
+}
+
+consumePromiseFive()
 
 
