c42b23901afbbfa8195aa643eb07afb9ba17c113
Mo Sameer <khansameer17275@gmail.com>
2026-05-11 07:21:41 +0000

    await

--- 09_advance_one/promises.js
+++ 09_advance_one/promises.js
@@ -79,7 +79,7 @@ async function getAllUsers(){
     try {
         const response = await fetch('https://jsonplaceholder.typicode.com/users')
 
-        const data = response.json()
+        const data = await response.json()
         console.log(data);
 
     } catch (error) {
