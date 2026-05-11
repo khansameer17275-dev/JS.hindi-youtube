7d3dd71c5df68026af9420ac916b2cd04b307e33
Mo Sameer <khansameer17275@gmail.com>
2026-05-11 07:19:46 +0000

    catch

--- 09_advance_one/promises.js
+++ 09_advance_one/promises.js
@@ -1,6 +1,5 @@
 //FileSystemDirectoryHandle('https://something.com').then().catch().finally()
 
-
 const promiseOne = new Promise(function(resolve, reject){
     // Do an async task
     setTimeout(function(){
@@ -66,11 +65,28 @@ const promiseFive = new Promise(function(resolve, reject) {
 })
 
 async function consumePromiseFive(){
+    try {
    const response = await promiseFive
    console.log(response);
-   
+    } catch (error){
+        console.log(error);
+    }
 }
 
 consumePromiseFive()
+
+async function getAllUsers(){
+    try {
+        const response = await fetch('https://jsonplaceholder.typicode.com/users')
+
+        const data = response.json()
+        console.log(data);
+
+    } catch (error) {
+        console.log("E: ", error);
+    }
+ }
+
+getAllUsers()
 
 
